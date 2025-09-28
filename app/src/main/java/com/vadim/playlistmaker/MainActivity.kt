d9daecfb9a70ff78a0e3b155fedd56ee94392f68
VadimBoz <werbel34@mail.ru>
2025-09-28 21:26:17 +0300

    v1

--- app/src/main/java/com/vadim/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/vadim/playlistmaker/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.vadim.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +22,58 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        val settingsBTN = findViewById<Button>(R.id.settings_BTN)
+        settingsBTN.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+            Log.d("TAG", "onClick: Settings button")
+        }
+
+        val clickListenerSearch = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Log.d("TAG", "onClick: Search button")
+                var intent = Intent(this@MainActivity, SearchActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        val searchBTN = findViewById<Button>(R.id.search_BTN)
+        searchBTN.setOnClickListener(clickListenerSearch)
+
+        val mediaBTN = findViewById<Button>(R.id.media_BTN)
+        mediaBTN.setOnClickListener {
+            Log.d("TAG", "onClick: Media button")
+            var intent = Intent(this@MainActivity, MediaActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("TAG", "Мы в методе onResume")
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("TAG", "Мы в методе onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("TAG", "Мы в методе onStop")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("TAG", "Мы в методе onStart")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("TAG", "Мы в методе onRestart")
+    }
+
 }
